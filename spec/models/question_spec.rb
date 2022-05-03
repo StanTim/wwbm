312e9d1a89047d7f551faead3e9c6ec879f7830c
StanTim <1694569@mail.ru>
2022-05-03 17:16:38 +0300

    add validate_uniqueness_of to question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -12,7 +12,10 @@ require 'rails_helper'
 # валидаций.
 RSpec.describe Question, type: :model do
   context 'validations check' do
+    subject { FactoryBot.build(:question) }
     it { should validate_presence_of :text }
+    it { should validate_uniqueness_of(:text) }
+
     it { should validate_presence_of :level }
 
     it { should validate_inclusion_of(:level).in_range(0..14) }
