e09cc7975d1474be0b00f4994793f044813a4cd7
StanTim <1694569@mail.ru>
2022-05-06 12:43:58 +0300

    add test for .correct_answer_key method

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -28,6 +28,10 @@ RSpec.describe GameQuestion, type: :model do
       # Именно под буквой b в тесте мы спрятали указатель на верный ответ
       expect(game_question.answer_correct?('b')).to be_truthy
     end
+
+    it 'correct .correct_answer_key' do
+      expect(game_question.correct_answer_key).to eq 'b'
+    end
   end
 
   context 'delegate .text, .level' do
