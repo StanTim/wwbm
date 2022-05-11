05662d732e8991f60095c315ee71e965aad18f3a
StanTim <1694569@mail.ru>
2022-05-11 09:49:59 +0300

    add model status test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -121,28 +121,82 @@ RSpec.describe Game, type: :model do
     end
 
     describe '#answer_current_question!' do
-      # Если пользователь дал верный ответ.
       let(:right_answer) { game_w_questions.current_game_question.correct_answer_key }
-      it 'when right answer' do
-        expect(game_w_questions.answer_current_question!(right_answer)).to be true
+      let(:wrong_answer) { (%w(a b c d) - [right_answer]).sample }
+
+      # Если пользователь дал верный ответ.
+      context 'when right answer' do
+        it 'should return true' do
+          expect(game_w_questions.answer_current_question!(right_answer)).to be true
+
+          l = game_w_questions.current_level
+          expect { game_w_questions.answer_current_question!(right_answer) }
+            .to change { game_w_questions.current_level }.from(l).to(l + 1)
+        end
+
+        it 'should тще change status' do
+          expect { game_w_questions.answer_current_question!(right_answer) }
+            .to_not change { game_w_questions.status }
+        end
       end
 
       # Если пользователь дал неправильный ответ.
-      let(:wrong_answer) { (%w(a b c d) - [right_answer]).sample }
-      it 'when answer is wrong' do
-        expect(game_w_questions.answer_current_question!(wrong_answer)).to be false
+      context 'when answer is wrong' do
+        it 'should be change is_failed to true' do
+          expect { game_w_questions.answer_current_question!(wrong_answer) }
+            .to change { game_w_questions.is_failed }.to(true)
+        end
+
+        it 'should be false' do
+          expect(game_w_questions.answer_current_question!(wrong_answer)).to be false
+        end
+
+        it 'should change status :fail' do
+          expect { game_w_questions.answer_current_question!(wrong_answer) }
+            .to change { game_w_questions.status }.from(:in_progress).to(:fail)
+        end
       end
 
-      # Правильный ответ на последний вопрос.
-      let(:current_level) { game_w_questions.new(current_level: Question::QUESTION_LEVELS.max) }
-      it 'when final question answer' do
-        expect(game_w_questions.answer_current_question!(right_answer)).to be true
+      # Правильный ответ на финальный вопрос.
+      context 'when final question answer' do
+        before { game_w_questions.current_level = Question::QUESTION_LEVELS.max }
+
+        it 'should be true' do
+          expect(game_w_questions.answer_current_question!(right_answer)).to be true
+        end
+
+        it 'should change game prize' do
+          expect { game_w_questions.answer_current_question!(right_answer) }
+            .to change { game_w_questions.prize }
+        end
+
+        it 'should change game prize to max value' do
+          expect { game_w_questions.answer_current_question!(right_answer) }
+            .to change { game_w_questions.prize }.to(Game::PRIZES.max)
+        end
+
+        it 'should be change finished_at' do
+          expect { game_w_questions.answer_current_question!(right_answer) }
+            .to change { game_w_questions.finished_at }
+        end
+
+        it 'should change status :won' do
+          expect { game_w_questions.answer_current_question!(right_answer) }
+            .to change { game_w_questions.status }.from(:in_progress).to(:won)
+        end
       end
 
       # Правильный ответ по завершении времени.
-      it 'when timeout' do
-      game_w_questions.created_at = (Game::TIME_LIMIT + 1.second).ago
-        expect(game_w_questions.answer_current_question!(right_answer)).to be false
+      context 'when timeout' do
+        before { game_w_questions.created_at = (Game::TIME_LIMIT + 1.second).ago }
+        it 'should be false' do
+          expect(game_w_questions.answer_current_question!(right_answer)).to be false
+        end
+
+        it 'should change status :timeout' do
+          expect { game_w_questions.answer_current_question!(right_answer) }
+            .to change { game_w_questions.status }.from(:in_progress).to(:timeout)
+        end
       end
     end
   end
